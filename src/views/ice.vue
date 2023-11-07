4435252074e9c2d9863ebdb588d3faef8db97ace
lwazino <lwaziworks@gmail.com>
2023-11-07 21:25:19 +0200

    update

---
+++ src/views/ice.vue
@@ -0,0 +1,135 @@
+<!-- <div
+        class="container w-60 bg-black p-2 rounded-lg hover:bg-zinc-700 hover:cursor-pointer"
+        @click="cardAselect('A1')"
+      >
+        <h2 class="text-sm font-medium text-white">Card A1</h2>
+
+        <div class="grid grid-cols-2 gap-2 mx-auto">
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 mt-4 bg-white"
+            :class="actionNames[cardA1[0]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 mt-4 bg-white"
+            :class="actionNames[cardA1[1]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 bg-white"
+            :class="actionNames[cardA1[2]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 bg-white"
+            :class="actionNames[cardA1[3]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 bg-white"
+            :class="actionNames[cardA1[4]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 bg-white"
+            :class="actionNames[cardA1[5]].class"
+          ></div>
+        </div>
+      </div>
+
+      <div
+        class="container w-60 bg-black p-2 rounded-lg hover:bg-zinc-700 hover:cursor-pointer"
+        @click="cardAselect('A2')"
+      >
+        <h2 class="text-sm font-medium text-white">Card A2</h2>
+
+        <div class="grid grid-cols-2 gap-2 mx-auto">
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 mt-4 bg-white"
+            :class="actionNames[cardA2[0]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 mt-4 bg-white"
+            :class="actionNames[cardA2[1]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 bg-white"
+            :class="actionNames[cardA2[2]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 bg-white"
+            :class="actionNames[cardA2[3]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 bg-white"
+            :class="actionNames[cardA2[4]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 bg-white"
+            :class="actionNames[cardA2[5]].class"
+          ></div>
+        </div>
+      </div>
+
+      <div
+        class="container w-60 bg-stone-400 border border-black p-2 rounded-lg hover:bg-stone-500 hover:cursor-pointer"
+        @click="cardBselect('B1')"
+      >
+        <h2 class="text-sm font-medium text-gray-900">Card B1</h2>
+
+        <div class="grid grid-cols-2 gap-2 mx-auto">
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 mt-4 bg-white"
+            :class="actionNames[cardB1[0]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 mt-4 bg-white"
+            :class="actionNames[cardB1[1]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 bg-white"
+            :class="actionNames[cardB1[2]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 bg-white"
+            :class="actionNames[cardB1[3]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 bg-white"
+            :class="actionNames[cardB1[4]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 bg-white"
+            :class="actionNames[cardB1[5]].class"
+          ></div>
+        </div>
+      </div>
+
+      <div
+        class="container w-60 bg-stone-400 border border-black p-2 rounded-lg hover:bg-stone-500 hover:cursor-pointer"
+        @click="cardBselect('B2')"
+      >
+        <h2 class="text-sm font-medium text-gray-900">Card B2</h2>
+
+        <div class="grid grid-cols-2 gap-2 mx-auto">
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 mt-4 bg-white"
+            :class="actionNames[cardB2[0]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 mt-4 bg-white"
+            :class="actionNames[cardB2[1]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 bg-white"
+            :class="actionNames[cardB2[2]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 bg-white"
+            :class="actionNames[cardB2[3]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 ml-2 bg-white"
+            :class="actionNames[cardB2[4]].class"
+          ></div>
+          <div
+            class="h-24 w-24 border border-white rounded-md p-2 bg-white"
+            :class="actionNames[cardB2[5]].class"
+          ></div>
+        </div>
+      </div> -->
